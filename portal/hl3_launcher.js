e6353853bbe4666e111aa22a071f4294b845eb57
bubbls <132022772+bubbls@users.noreply.github.com>
2025-12-01 15:40:48 -0700

    maybe

--- portal/hl3_launcher.js
+++ portal/hl3_launcher.js
@@ -47,211 +47,208 @@ if (ENVIRONMENT_IS_NODE) {
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
 // include: emscripten/pre.js
-Module['arguments'] = Module['arguments'] || []
-Module['arguments'].push(
-  '-game', 'portal',
-  '-noip',
-  '-language', 'english',
-  '-windowed',
-  '+mat_hdr_level', '0',
-  '+mat_colorcorrection', '1'
-)
+    const CDN_BASE = 'https://cdn.jsdelivr.net/gh/bubbls/UGS-Assets@main/portal/';
+    
+    Module['arguments'] = Module['arguments'] || []
+    Module['arguments'].push(
+      '-game', 'portal',
+      '-noip',
+      '-language', 'english',
+      '-windowed',
+      '+mat_hdr_level', '0',
+      '+mat_colorcorrection', '1'
+    )
 
-class DataLoader {
-  mapsOrdered = [
-    'background1',
-    'testchmb_a_00',
-    'testchmb_a_01',
-    'testchmb_a_02',
-    'testchmb_a_03',
-    'testchmb_a_04',
-    'testchmb_a_05',
-    'testchmb_a_06',
-    'testchmb_a_07',
-    'testchmb_a_08',
-    'testchmb_a_09',
-    'testchmb_a_10',
-    'testchmb_a_11',
-    'testchmb_a_13',
-    'testchmb_a_14',
-    'testchmb_a_15'
-  ]
+    class DataLoader {
+      mapsOrdered = [
+        'background1',
+        'testchmb_a_00',
+        'testchmb_a_01',
+        'testchmb_a_02',
+        'testchmb_a_03',
+        'testchmb_a_04',
+        'testchmb_a_05',
+        'testchmb_a_06',
+        'testchmb_a_07',
+        'testchmb_a_08',
+        'testchmb_a_09',
+        'testchmb_a_10',
+        'testchmb_a_11',
+        'testchmb_a_13',
+        'testchmb_a_14',
+        'testchmb_a_15'
+      ]
 
-  loadedMaps = {}
+      loadedMaps = {}
 
-  async loadMapWithDeps(mapName) {
-    const index = this.mapsOrdered.indexOf(mapName)
-    if (index === -1) {
-      throw new Error(`no such map: ${mapName}`)
-    }
+      async loadMapWithDeps(mapName) {
+        const index = this.mapsOrdered.indexOf(mapName)
+        if (index === -1) {
+          throw new Error(`no such map: ${mapName}`)
+        }
 
-    // load past maps and current one
-    for (let i = 0; i < index + 1; i++) {
-      await this.loadMapCached(this.mapsOrdered[i])
-    }
+        for (let i = 0; i < index + 1; i++) {
+          await this.loadMapCached(this.mapsOrdered[i])
+        }
 
-    // schedule next map if it exists
-    const next = this.mapsOrdered[index + 1]
-    if (next) {
-      this.loadMapCached(next)
-    }
-  }
-
-  async loadMapCached(mapName) {
-    if (mapName in this.loadedMaps) return this.loadedMaps[mapName]
-    const promise = this.loadMap(mapName)
-    this.loadedMaps[mapName] = promise
-    return promise
-  }
-
-  async setProgress(mapName, progress) {
-    if (progress < 1) {
-      spinnerElement.style.display = ''
-      statusElement.innerText = `Downloading map ${mapName}`
-      progressElement.hidden = false
-      progressElement.value = progress
-    } else {
-      spinnerElement.style.display = 'none'
-      statusElement.innerText = ''
-      progressElement.hidden = true
-    }
-  }
-
-  async loadMap(mapName) {
-    this.setProgress(mapName, 0)
-
-    let resolve, reject
-    const promise = new Promise((res, rej) => {
-      resolve = res;
-      reject = rej
-    })
-
-    // Special handling for background1 which is split into 7 parts
-    if (mapName === 'background1') {
-      this.loadMapParts(mapName, 7, resolve, reject)
-      return promise
-    }
-
-    const xhr = new XMLHttpRequest()
-    xhr.responseType = 'arraybuffer'
-    xhr.onprogress = e => {
-      this.setProgress(mapName, e.loaded / e.total)
-    }
-
-    xhr.onerror = () => {
-      reject(new Error(`cannot load map ${mapName}`))
-    }
-
-    xhr.onload = e => {
-      this.setProgress(mapName, 1)
-      this.processMapData(xhr.response, resolve)
-    }
-    xhr.open('GET', `chunks/${mapName}.data`, true)
-    xhr.send()
-
-    return promise
-  }
-
-  async loadMapParts(mapName, numParts, resolve, reject) {
-    const partProgress = new Array(numParts).fill(0)
-    const partTotals = new Array(numParts).fill(0)
-    const partData = new Array(numParts).fill(null)
-    let completedParts = 0
-
-    const updateProgress = () => {
-      const totalLoaded = partProgress.reduce((sum, val) => sum + val, 0)
-      const totalSize = partTotals.reduce((sum, val) => sum + val, 0)
-      if (totalSize > 0) {
-        this.setProgress(mapName, totalLoaded / totalSize)
+        const next = this.mapsOrdered[index + 1]
+        if (next) {
+          this.loadMapCached(next)
+        }
       }
-    }
 
-    const checkComplete = () => {
-      if (completedParts === numParts) {
-        // Combine all parts into a single ArrayBuffer
-        const totalLength = partData.reduce((sum, data) => sum + data.byteLength, 0)
-        const combined = new Uint8Array(totalLength)
-        
-        let offset = 0
+      async loadMapCached(mapName) {
+        if (mapName in this.loadedMaps) return this.loadedMaps[mapName]
+        const promise = this.loadMap(mapName)
+        this.loadedMaps[mapName] = promise
+        return promise
+      }
+
+      async setProgress(mapName, progress) {
+        if (progress < 1) {
+          spinnerElement.style.display = ''
+          statusElement.innerText = `Downloading map ${mapName}`
+          progressElement.hidden = false
+          progressElement.value = progress
+        } else {
+          spinnerElement.style.display = 'none'
+          statusElement.innerText = ''
+          progressElement.hidden = true
+        }
+      }
+
+      async loadMap(mapName) {
+        this.setProgress(mapName, 0)
+
+        let resolve, reject
+        const promise = new Promise((res, rej) => {
+          resolve = res;
+          reject = rej
+        })
+
+        if (mapName === 'background1') {
+          this.loadMapParts(mapName, 7, resolve, reject)
+          return promise
+        }
+
+        const xhr = new XMLHttpRequest()
+        xhr.responseType = 'arraybuffer'
+        xhr.onprogress = e => {
+          this.setProgress(mapName, e.loaded / e.total)
+        }
+
+        xhr.onerror = () => {
+          reject(new Error(`cannot load map ${mapName}`))
+        }
+
+        xhr.onload = e => {
+          this.setProgress(mapName, 1)
+          this.processMapData(xhr.response, resolve)
+        }
+        // USE CDN URL
+        xhr.open('GET', `${CDN_BASE}chunks/${mapName}.data`, true)
+        xhr.send()
+
+        return promise
+      }
+
+      async loadMapParts(mapName, numParts, resolve, reject) {
+        const partProgress = new Array(numParts).fill(0)
+        const partTotals = new Array(numParts).fill(0)
+        const partData = new Array(numParts).fill(null)
+        let completedParts = 0
+
+        const updateProgress = () => {
+          const totalLoaded = partProgress.reduce((sum, val) => sum + val, 0)
+          const totalSize = partTotals.reduce((sum, val) => sum + val, 0)
+          if (totalSize > 0) {
+            this.setProgress(mapName, totalLoaded / totalSize)
+          }
+        }
+
+        const checkComplete = () => {
+          if (completedParts === numParts) {
+            const totalLength = partData.reduce((sum, data) => sum + data.byteLength, 0)
+            const combined = new Uint8Array(totalLength)
+            
+            let offset = 0
+            for (let i = 0; i < numParts; i++) {
+              combined.set(new Uint8Array(partData[i]), offset)
+              offset += partData[i].byteLength
+            }
+
+            this.setProgress(mapName, 1)
+            this.processMapData(combined.buffer, resolve)
+          }
+        }
+
         for (let i = 0; i < numParts; i++) {
-          combined.set(new Uint8Array(partData[i]), offset)
-          offset += partData[i].byteLength
+          const partNum = i + 1
+          const partIndex = i
+
+          const xhr = new XMLHttpRequest()
+          xhr.responseType = 'arraybuffer'
+          
+          xhr.onprogress = (e) => {
+            if (e.lengthComputable) {
+              partProgress[partIndex] = e.loaded
+              partTotals[partIndex] = e.total
+              updateProgress()
+            }
+          }
+
+          xhr.onerror = () => {
+            reject(new Error(`cannot load ${mapName}.data.part${partNum}`))
+          }
+
+          xhr.onload = () => {
+            partData[partIndex] = xhr.response
+            completedParts++
+            checkComplete()
+          }
+
+          // USE CDN URL
+          xhr.open('GET', `${CDN_BASE}chunks/${mapName}.data.part${partNum}`, true)
+          xhr.send()
+        }
+      }
+
+      processMapData(arrayBuffer, resolve) {
+        const dv = new DataView(arrayBuffer)
+        let offset = 0
+
+        while (offset < dv.byteLength) {
+          const pathLen = dv.getInt32(offset, true)
+          const dataLen = dv.getInt32(offset + 4, true)
+          const path = new TextDecoder().decode(new DataView(
+            dv.buffer,
+            offset + 8,
+            pathLen
+          ))
+          const blob = new Uint8Array(
+            dv.buffer,
+            offset + 8 + pathLen,
+            dataLen
+          )
+          offset += 8 + pathLen + dataLen
+
+          const dir = path.replace(/\/[^\/]+$/, '')
+          FS.mkdirTree(dir)
+          FS.writeFile(path, blob)
         }
 
-        this.setProgress(mapName, 1)
-        this.processMapData(combined.buffer, resolve)
+        resolve()
       }
     }
 
-    // Load all parts
-    for (let i = 0; i < numParts; i++) {
-      const partNum = i + 1
-      const partIndex = i
+    const dataLoader = new DataLoader()
 
-      const xhr = new XMLHttpRequest()
-      xhr.responseType = 'arraybuffer'
-      
-      xhr.onprogress = (e) => {
-        if (e.lengthComputable) {
-          partProgress[partIndex] = e.loaded
-          partTotals[partIndex] = e.total
-          updateProgress()
-        }
-      }
-
-      xhr.onerror = () => {
-        reject(new Error(`cannot load ${mapName}.data.part${partNum}`))
-      }
-
-      xhr.onload = () => {
-        partData[partIndex] = xhr.response
-        completedParts++
-        checkComplete()
-      }
-
-      xhr.open('GET', `chunks/${mapName}.data.part${partNum}`, true)
-      xhr.send()
+    Module.downloadMap = (lock, mapName) => {
+      dataLoader.loadMapWithDeps(mapName).then(() => {
+        Atomics.store(HEAP32, lock, 0)
+        Atomics.notify(HEAP32, lock)
+      })
     }
-  }
-
-  processMapData(arrayBuffer, resolve) {
-    const dv = new DataView(arrayBuffer)
-
-    let offset = 0
-
-    // data format: { pathLen: uint32le, dataLen: uint32le, path: bytes, blob: bytes }[]
-    while (offset < dv.byteLength) {
-      const pathLen = dv.getInt32(offset, true)
-      const dataLen = dv.getInt32(offset + 4, true)
-      const path = new TextDecoder().decode(new DataView(
-        dv.buffer,
-        offset + 8,
-        pathLen
-      ))
-      const blob = new Uint8Array(
-        dv.buffer,
-        offset + 8 + pathLen,
-        dataLen
-      )
-      offset += 8 + pathLen + dataLen
-
-      const dir = path.replace(/\/[^\/]+$/, '')
-      FS.mkdirTree(dir)
-      FS.writeFile(path, blob)
-    }
-
-    resolve()
-  }
-}
-
-const dataLoader = new DataLoader()
-
-Module.downloadMap = (lock, mapName) => {
-  dataLoader.loadMapWithDeps(mapName).then(() => {
-    Atomics.store(HEAP32, lock, 0)
-    Atomics.notify(HEAP32, lock)
-  })
-}
 
 
 var arguments_ = [];
