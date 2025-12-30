2498e16e6a4855dd5c2bc1adfdfe0dbeb5a80785
mcMatty o'Briore <kitloverbs90210@gmail.com>
2025-12-30 17:38:31 -0600

    Delete aviamaster directory

--- aviamaster/html/Aviamasters/loader.js
+++
@@ -1,5 +0,0 @@
-window.__OPTIONS__.game_bundle_source = '';
-const script = window.document.createElement('script');
-script.type = 'module';
-window.document.body.append(script);
-script.src = (window.__OPTIONS__.resources_path + '/assets/index-BUV3pfE2.js');
